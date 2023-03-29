62743178e258aa67a1e421c0c9b62bf1d6f2c066
Kudums <kudumsendijs@gmail.com>
2023-03-29 16:05:08 +0300

    Uzsākts darbs ar image objektiem

---
+++ Assets/VardaParadisana.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//nepiecasams lai stradut ar UI elementiem
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour {
+	/*int kadri = 0;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Programma tika starteta!");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Kadrs: " + kadri);
+		kadri++;
+
+		if (kadri == 10) {
+			Debug.Log ("Tu sasniedzi 10. kadru!");
+		}
+	}*/
+
+	//Mainígais kura uzlgabás tekstu no input field
+	private string teksts;
+	//Lauks, kura ievada tekstu
+	public GameObject ievadesLauks;
+	//text objekts, kura attelos tekstu pec pogas nnospiesanas
+	public GameObject TekstaLauks1;
+	//masivs ar iespejamajiem teksta sakumiem
+	private string[] varianti = 
+	{"Sveiki ","Labdien ","Prieks tevi redzet ","Uzredzesanos "};
+	//mainigais uzglabas random skaiti
+	int nejausais;
+
+	public void NolasitTekstu(){
+		teksts = ievadesLauks.GetComponent<Text>().text;
+		nejausais = Random.Range (0, varianti.Length);
+		TekstaLauks1.GetComponent<Text>().text = varianti[nejausais]+teksts.ToUpper()+"!";
+	}
+}
--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Unity projekts kas demonstrē UI elementu lietojumu par tēmu: MR. Bean
 ![MR.Bean attēls](https://th.bing.com/th/id/R.92f9bb31e62a10adc9a79bf79ece8a19?rik=LOsJ%2fUrK2IaFyQ&pid=ImgRaw&r=0)
 
 ## Darāmo darbu
-- [ ] Input field objekta lietojums projektā
-- [ ] Text objekta lietojums projektā
-- [ ] Button objekta lietojums projektā
+- [x] Input field objekta lietojums projektā
+- [x] Text objekta lietojums projektā
+- [x] Button objekta lietojums projektā
 - [ ] Image objekta lietojums projektā
 - [ ] Toggle objekta lietojums projektā
 - [ ] Toggle group elementa lietojums projektā
-- [ ] C# skriptu izveide un piesaiste objektiem
+- [x] C# skriptu izveide un piesaiste objektiem
 - [ ] Drop down objekta lietojums projektā
 - [ ] Slider objekta lietojums projektā
 - [ ] Audio source objekta lietojums projektā
